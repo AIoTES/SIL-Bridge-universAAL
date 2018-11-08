a33f4bda3ae2df5684b6fa8b6f3f5d9941fb3bf7
Alvaro Fides <alfiva@upvnet.upv.es>
2018-11-08 14:06:35 +0100

    Handle non-success responses from uAAL
    In query and listDevices

--- src/main/java/eu/interiot/intermw/bridge/uaal/UAALBridge.java
+++ src/main/java/eu/interiot/intermw/bridge/uaal/UAALBridge.java
@@ -72,6 +72,9 @@ public class UAALBridge extends AbstractBridge {
     private static final String URI_PARAM = "http://www.daml.org/services/owl-s/1.1/Process.owl#parameterValue";
     private static final String URI_OUTPUT = "http://ontology.universAAL.org/InterIoT.owl#output1";
     private static final String URI_EVENT = "http://ontology.universAAL.org/Context.owl#ContextEvent";
+    private static final String URI_STATUS = "http://ontology.universAAL.org/uAAL.owl#callStatus";
+    private static final String URI_SUCCEEDED = "http://ontology.universAAL.org/uAAL.owl#call_succeeded";
+    private static final String URI_NOMATCH = "http://ontology.universAAL.org/uAAL.owl#no_matching_service_found";
     private final Logger log = LoggerFactory.getLogger(UAALBridge.class);
     private String url;
     private String usr;
@@ -387,6 +390,16 @@ public class UAALBridge extends AbstractBridge {
 		.post(url + "spaces/" + space + "/service/callers/" + DEFAULT_CALLER, usr, pwd, TEXT, body);
 	Model jena = ModelFactory.createDefaultModel();
 	Model result = ModelFactory.createDefaultModel();
+	if(!jena.contains(null, jena.getProperty(URI_STATUS), jena.getResource(URI_SUCCEEDED))){
+	    if(!jena.contains(null, jena.getProperty(URI_STATUS), jena.getResource(URI_NOMATCH))){
+		log.warn("Could not find devices in uAAL because there is no one answering to the request");
+		responseMsg.setPayload(new IoTDevicePayload(result));
+		responseMsg.getMetadata().setStatus("OK");
+		return responseMsg;
+	    }
+	    log.warn("Could not find devices in uAAL because there was an error");
+	    return error(msg);
+	}
 	jena.read(new ByteArrayInputStream(serviceResponse.getBytes()), null, "TURTLE");
 	// The output is itself a serialized device TODO prevent multivalues
 	String output=jena.getRequiredProperty(
@@ -422,6 +435,16 @@ public class UAALBridge extends AbstractBridge {
 	Model jena = ModelFactory.createDefaultModel();
 	Model result = ModelFactory.createDefaultModel();
 	jena.read(new ByteArrayInputStream(serviceResponse.getBytes()), null, "TURTLE");
+	if(!jena.contains(null, jena.getProperty(URI_STATUS), jena.getResource(URI_SUCCEEDED))){
+	    if(!jena.contains(null, jena.getProperty(URI_STATUS), jena.getResource(URI_NOMATCH))){
+		log.warn("Could not find devices in uAAL because there is no one answering to the request");
+		responseMsg.setPayload(new IoTDevicePayload(result));
+		responseMsg.getMetadata().setStatus("OK");
+		return responseMsg;
+	    }
+	    log.warn("Could not find devices in uAAL because there was an error");
+	    return error(msg);
+	}
 	ResIterator roots = jena.listSubjectsWithProperty(RDF.type, URI_MULTI);
 	if (roots.hasNext()) { // Many responses aggregated into one
 	    Resource root = roots.next();
