bfd419866473fa827a8a16eea5a50b25b116ed24
Matilde <majuse@upv.es>
2019-07-25 11:32:31 +0200

    Fixed bridge management scripts

--- src/main/script/install.sh
+++ src/main/script/install.sh
@@ -2,5 +2,5 @@
 #TODO check volumes are populated
 cp -rv /mngt/dep/* /volume/bin/ROOT/WEB-INF/lib
 cp -rv /mngt/bin/* /volume/bin/ROOT/WEB-INF/lib
-cp -rv /mngt/config/* /volume/config/
+cp -rv /mngt/config/* /volume/config
 #TODO restart inter-mw
